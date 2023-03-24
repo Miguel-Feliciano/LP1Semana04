64d264f84a21627e4fb9fda6d89db3bb4944a926
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-24 14:56:13 +0000

    altered PowersOf2Until5 to call PowersOf2UntilN. Completed exercise 4

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -8,13 +8,20 @@ namespace Power2Method
         {
             PowersOf2Until5();
             PowersOf2Until5();
+            PowersOf2UntilN(7);
+            PowersOf2UntilN(4);
         }
         private static void PowersOf2Until5()
+    {
+        PowersOf2UntilN(5); 
+    }
+
+    private static void PowersOf2UntilN(int n)
+    {
+        for (int i = 1; i <= (1 << n); i = i << 1)
         {
-            for (int i = 1; i <= (1 << 5); i = i << 1)
-            {
-                Console.WriteLine(i);
-            }
+            Console.WriteLine(i);
         }
+    }
     }
 }
