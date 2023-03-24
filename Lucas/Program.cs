65a4419dcb6a341553c0e272b62ff6bb92faf14d
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-24 15:22:39 +0000

    Completed exercise 7

---
+++ Lucas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lucas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = Lucas(n);
+            Console.WriteLine($"the {n} number of the Lucas sequence is: {resultado}");
+        }
+
+        private static int Lucas(int n)
+        {
+            if (n == 0)
+            {
+                return 2;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Lucas(n - 1) + Lucas(n - 2);
+            }
+        }
+    }
+}
